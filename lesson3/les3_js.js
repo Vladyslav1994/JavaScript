3d121fa2e6a793ccfed6adebb1db3899fd9bc492
Vladyslav1994 <bilokin94@gmail.com>
2025-02-05 15:02:26 +0000

    1

--- lesson3/les3_js.js
+++ lesson3/les3_js.js
@@ -82,29 +82,29 @@
 //
 // #4WrHwFTEop0
 // є масив
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
 // за допомоги циклу вивести:
 //     - користувачів зі статусом true
 // - користувачів зі статусом false
 // - користувачів які старші за 30 років
-for (let user of users) {
-    if (user.status === true){
-        console.log(`user ${user.name} is ${user.status}`);
-    }if (user.status === false){
-        console.log(`user ${user.name} is ${user.status}`);
-    }if (user.age > 30){
-        console.log(`user ${user.name} is ${user.age}`);
-    }
-}
+// for (let user of users) {
+//     if (user.status === true){
+//         console.log(`user ${user.name} is ${user.status}`);
+//     }if (user.status === false){
+//         console.log(`user ${user.name} is ${user.status}`);
+//     }if (user.age > 30){
+//         console.log(`user ${user.name} is ${user.age}`);
+//     }
+// }
