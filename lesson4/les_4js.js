ea1fd17c6184311c24eccecad5e24c2edd241b1f
Vladyslav1994 <bilokin94@gmail.com>
2025-02-07 20:55:07 +0000

    1

--- lesson4/les_4js.js
+++ lesson4/les_4js.js
@@ -88,24 +88,60 @@
 
 //     #pghbnSB
 // - створити функцію яка повертає найменьше число з масиву
-let arr = [3,5,8,11,2,4]
-
-function f(array) {
-    let number = array[0];
-    for (let arr of array) {
-        if (arr < number) {
-            number = arr
-        }
-    }
-    return number
-}
-
-console.log(f(arr))
+// let arr = [3,5,8,11,2,4]
+//
+// function f(array) {
+//     let number = array[0];
+//     for (let arr of array) {
+//         if (arr < number) {
+//             number = arr
+//         }
+//     }
+//     return number
+// }
+//
+// console.log(f(arr))
 // #EKRNVPM
 // - створити функцію sum(arr) яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
+// це була моя версія до якої дійшов і не зміг зрозуміти чому не працює, подивився відео і подабчив помилку
+// let array = [3,5,8,11,2,4]
+// function f (arr){
+//     let b = 0
+//     for (let a of arr) {
+//         console.log(a+b)
+//     }
+//     return b
+// }
+// let s = f(array)
+// console.log(s)
+
+// let array = [3,5,8,11,2,4]
+// function f (arr){
+//     let b = 0
+//     for (let a of arr) {
+//         b = a + b
+//     }
+//     return b
+// }
+// let s = f(array)
+// console.log(s)
+
 // #kpsbSQCt2Lf
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відповідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+// let array = [11,22,33,44]
+
 // #mkGDenYnNjn
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD') // => 400
+
+function usd(sum) {
+    return sum / 25
+}
+function eur(sum) {
+    return sum / 42
+}
+let value_usd = usd(500)
+console.log(value_usd)
+let value_eur = eur(500)
+console.log(value_eur)
