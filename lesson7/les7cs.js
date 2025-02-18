3961197e0de37370ff69a88e8b937663ee455ad0
Vladyslav1994 <bilokin94@gmail.com>
2025-02-18 09:06:38 +0000

    1

---
+++ lesson7/les7cs.js
@@ -0,0 +1,83 @@
+// #XjJuucOMR0
+// - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
+// створити пустий масив, наповнити його 10 об'єктами new User(....)
+let users = []
+class User {
+    constructor(id, name, surname, email, phone) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+    }
+}
+
+// let user1 = new User(1, 'anton', 'bereza', 'fjaskfnaj@dsakkda', 1321321)
+// console.log(user1);
+users.push(new User(1, 'anton', 'bereza', 'fjaskfnaj@dsakkda', 1321321));
+users.push(new User(2, 'masha', 'komso', 'fjaskfnaj@dsakkda', 1321321));
+users.push(new User(3, 'ania', 'ostro', 'fjaskfnaj@dsakkda', 1321321));
+users.push(new User(4, 'katia', 'isko', 'fjaskfnaj@dsakkda', 1321321));
+users.push(new User(5, 'lila', 'lestenko', 'fjaskfnaj@dsakkda', 1321321));
+users.push(new User(6, 'vitia', 'pustowski', 'fjaskfnaj@dsakkda', 1321321));
+users.push(new User(7, 'petya', 'prusko', 'fjaskfnaj@dsakkda', 1321321));
+users.push(new User(8, 'andrew', 'mosko', 'fjaskfnaj@dsakkda', 1321321));
+users.push(new User(9, 'sasha', 'fesko', 'fjaskfnaj@dsakkda', 1321321));
+users.push(new User(10, 'nastya', 'arko', 'fjaskfnaj@dsakkda', 1321321));
+
+// console.log(users);
+//
+// #2ikXsE2WiKZ
+// - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
+// let xxx = users.filter((value) => {
+//     return value.id % 2 === 0;
+// });
+// console.log(xxx);
+//
+// #pOeHKct
+// - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
+users.sort(function (a, b) {
+    // return a.id - b.id; зростання
+    // return b.id - a.id; зменшення
+})
+console.log(users);
+//
+// #nkMXISv
+// - створити конструктор для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
+// створити пустий масив, наповнити його 10 об'єктами Client
+//
+//
+// #8abtVjRv
+// - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
+//
+//
+// #vV9a6584I5
+// - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+// -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+//
+//
+// #5kla3yMpgp
+// - (Те саме, тільки через клас)
+// Створити клас який дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+// -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+//
+//
+//
+// #zg6Fifnqig
+// -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
+// Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
+//     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
+//     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
+//
+//
+//
+// #gsKLAsNWM
+// *Через Array.prototype. створити власний foreach, filter
